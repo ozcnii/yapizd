4e0cbe55b253d116670c9e6a082da9cad559af0b
ozcnii <ercount89@gmail.com>
2024-04-08 13:37:50 +0400

    asd

--- Lab6/obj/Debug/net8.0/Lab6.AssemblyInfo.cs
+++ Lab6/obj/Debug/net8.0/Lab6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a436ed435b77d784ee867bafd3155f1669b5b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99117336f05a71c124ae75ceb49dda4b92996d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
